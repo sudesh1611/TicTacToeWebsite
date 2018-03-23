c173c5c5a64347c1f6fb9846ccd296af35f043f8
sudesh1611 <sudesh1611@gmail.com>
2018-03-23 12:46:30 +0530

    Added Profile Statistics Page and Account Deactivation Page

---
+++ TicTacToeWebsite/TicTacToeWebsite/Pages/DeActivateAccount.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeWebsite.Controllers;
+using TicTacToeWebsite.Data;
+
+namespace TicTacToeWebsite.Pages
+{
+    public class DeActivateAccountModel : PageModel
+    {
+        [BindProperty]
+        public string Password { get; set; }
+
+        private readonly UserAccountDbContext _userContext;
+
+        public DeActivateAccountModel(UserAccountDbContext context)
+        {
+            _userContext = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostAsync(string returnURL=null)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string emaiID = User.FindFirst(ClaimTypes.Email).Value;
+                var user = await _userContext.UserAccount.SingleOrDefaultAsync(m => (m.EmailID == emaiID));
+                if (user != null)
+                {
+                    if(user.Password!=Password)
+                    {
+                        ModelState.AddModelError(string.Empty, "Password is Wrong!");
+                        return Page();
+                    }
+                    else
+                    {
+                        _userContext.UserAccount.Remove(user);
+                        await _userContext.SaveChangesAsync();
+                        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                        return RedirectToPage("/Index");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "User Already Deleted!!");
+                    return Page();
+                }
+            }
+            return Page();
+        }
+    }
+}
---
+++ TicTacToeWebsite/TicTacToeWebsite/Pages/Statistics.cshtml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TicTacToeWebsite.Data;
+
+namespace TicTacToeWebsite.Pages
+{
+    public class StatisticsModel : PageModel
+    {
+
+        private readonly UserAccountDbContext _userContext;
+
+        public class InputModel
+        {
+            public string UserName { get; set; }
+            public string FullName { get; set; }
+            public long SingleEasyWins { get; set; }
+            public long SingleEasyLoses { get; set; }
+            public long SingleEasyTies { get; set; }
+            public long SingleHardWins { get; set; }
+            public long SingleHardLoses { get; set; }
+            public long SingleHardTies { get; set; }
+            public long DoubleWins { get; set; }
+            public long DoubleLoses { get; set; }
+            public long DoubleTies { get; set; }
+            public string EmailID { get; set; }
+            public long TotalPlayed { get; set; }
+        }
+
+        [BindProperty]
+        public InputModel inputModel { get; set; }
+
+        public StatisticsModel(UserAccountDbContext context)
+        {
+            _userContext = context;
+        }
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                string emaiID = User.FindFirst(ClaimTypes.Email).Value;
+                var user = await _userContext.UserAccount.SingleOrDefaultAsync(m => (m.EmailID == emaiID));
+                if (user != null)
+                {
+                    inputModel = new InputModel
+                    {
+                        UserName = user.UserName,
+                        FullName = user.FullName,
+                        EmailID = user.EmailID,
+                        SingleEasyLoses = user.SingleEasyLoses,
+                        SingleEasyTies = user.SingleEasyTies,
+                        SingleEasyWins = user.SingleEasyWins,
+                        SingleHardLoses = user.SingleHardLoses,
+                        SingleHardTies = user.SingleHardTies,
+                        SingleHardWins = user.SingleHardWins,
+                        DoubleLoses = user.DoubleLoses,
+                        DoubleTies = user.DoubleTies,
+                        DoubleWins = user.DoubleWins,
+                        TotalPlayed = user.SingleEasyLoses + user.SingleEasyTies + user.SingleEasyWins + user.SingleHardLoses + user.SingleHardTies + user.SingleHardWins + user.DoubleLoses + user.DoubleTies + user.DoubleWins
+                    };
+                }
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Error");
+            }
+        }
+    }
+}
